1cf23e8931826d245e6fb514a07cb51bfdf09349
unknown <Cetecom@SJAPCA03LC1017.duoc.acad>
2026-04-22 21:29:06 -0400

    Primer commit

---
+++ src/main/java/cl/duoc/tintero_social/TinteroSocialApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.tintero_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinteroSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinteroSocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/duoc/tintero_social/TinteroSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.tintero_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinteroSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
